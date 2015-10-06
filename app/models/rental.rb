3830c7abe771cef27af2eff6c4c8d9d4c84d428d
Brian Sharp <briansharp1994@gmail.com>
2015-10-06 13:41:01 -0400

    updated models

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,4 @@
 class Rental < ActiveRecord::Base
+	belongs_to :user
+	has_one :equipment
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	has_many :rentals
+	has_many :events
+
+	scope :sorted, lambda { order("users.last_name ASC")}
+	
 end
