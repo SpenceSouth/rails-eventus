eb9bef8d085bfdd30d02d51cfa303e3a53951149
Brian D. Sharp <briansharp1994@gmail.com>
2015-11-06 14:00:07 -0500

    requests page now displays seperate glypicons for events and rentals

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,13 @@
 class RequestsController < ApplicationController
   def show
-  	@eventRequests = Event.all.where("pending_approval = ?", true)
-  	@rentalRequests = Rental.all.where("pending_approval = ?", true)
+  	eventRequests = Event.all.where("pending_approval = ?", true)
+  	rentalRequests = Rental.all.where("pending_approval = ?", true)
+  	@getSortedRequests = eventRequests + rentalRequests
   	@users = User.all
   end
+
+  def getSortedRequests
+  	
+  end
+
 end
