f31aaea35354953e719b13d7d4aba799185dabe5
Brian D. Sharp <briansharp1994@gmail.com>
2015-11-06 22:14:52 -0500

    Changed scroll bars

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,11 +1,12 @@
 function initEventPanel(){
 	
 	$(".event-panel").each(function( index ){
-		var offset = index * 270 + 20;
+		var offset = index * 270;
 		var offsetString = offset.toString() + 'px';
 		$(this).css('left',offsetString);
-
 	});
+	var width = ($(".event-panel").size() * 270);
+	$("#slide-panel hr").css('width', width);
 }
 
 $(document).ready(function(){
